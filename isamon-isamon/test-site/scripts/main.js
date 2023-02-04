041db99ab78f2baa0f67c669154ad3e3c6cd4965
isamon-isamon <116419364+isamon-isamon@users.noreply.github.com>
2023-02-04 17:33:56 +0900

    Update main.js

--- isamon-isamon/test-site/scripts/main.js
+++ isamon-isamon/test-site/scripts/main.js
@@ -2,3 +2,20 @@ const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
 
+function message() {
+  document.getElementById("view_time").innerHTML = getNow();
+}
+
+function getNow() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var mon = now.getMonth()+1; //１を足すこと
+	var day = now.getDate();
+	var hour = now.getHours();
+	var min = now.getMinutes();
+	var sec = now.getSeconds();
+
+	//出力用
+	var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒"; 
+	return s;
+}
