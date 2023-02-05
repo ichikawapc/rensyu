d964e338c075d12165c1e6d29e41a3c092ea8a83
Kazu <k6990667@gmail.com>
2023-02-06 04:47:17 +0900

    timer image change

--- kazuweb/test-site/scripts/main.js
+++ kazuweb/test-site/scripts/main.js
@@ -1,9 +1,45 @@
-/*const myHeading = document.querySelector('h1');
+/*
+const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 */
 
-let counter = 0;
-let mongon = ''　
+let click_count = 0;
+let mongon = ''
+
+let count = 1;
+let display = "Counter: " + count;
+
+function incr() {
+    count = count + 1;
+    display = display + " "+ count
+}
+
+function show() {    
+    change_image();
+    //const counter = document.querySelector('#counter');
+    //counter.textContent = display;
+}
+
+function change_image(){
+  var arrIm = ["images/apple.jpg",
+                "images/IMG_1052.jpg"];
+  const myImage = document.querySelector('img');
+  myImage.setAttribute('src',arrIm[count%2]);
+}
+
+function work() {
+    //show();
+    const ms = 3000; //3000 ミリ秒
+    new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, ms)
+    }).then(() => {
+        incr();
+        show();
+        work();
+    });
+}
 
 function isPrime(n) {
   if (n <= 1) return false;
@@ -13,21 +49,27 @@ function isPrime(n) {
   return true;
 }
 
+
+window.onload = function() {
+    work();
+}
+
+
 document.querySelector('img').addEventListener('click', function () {
-  　counter = counter + 1;
-    
+    click_count = click_count + 1;
     mongon = mongon +'痛っ!';
-    if (counter % 2 === 0){
-        alert(mongon +counter+'   2の倍数だよ');
-    }else if(counter % 3 === 0){
-        alert(mongon +counter+'  3の倍数だよ');
-    }else if(counter === 5){
+    if (click_count % 2 === 0){
+        change_image();
+        alert(mongon +click_count+'   2の倍数だよ');
+    }else if(click_count % 3 === 0){
+        alert(mongon +click_count+'  3の倍数だよ');
+    }else if(click_count === 5){
         const myImage = document.querySelector('img');
         myImage.setAttribute('src','images/apple.jpg');
-        alert(counter+'画像が変わった！！！');
+        alert(click_count+'画像が変わった！！！');
     }
-    if(isPrime(counter)===true){
-       alert(mongon +counter+'  素数だよ');
+    if(isPrime(click_count)===true){
+       alert(click_count+'  素数だよ');
       }
 });
 
