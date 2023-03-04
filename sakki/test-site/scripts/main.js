6773414ebf3d13c50772379da39564060e8a8453
sakki594 <116419266+sakki594@users.noreply.github.com>
2023-03-04 15:47:55 +0900

    Update main.js

--- sakki/test-site/scripts/main.js
+++ sakki/test-site/scripts/main.js
@@ -1,7 +1,11 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
-
+/*
+変数の宣言をする
+*/
 let myVariable;
 myVariable = 'Bob';
 myVariable;
 
+// Steveに変更する
+myVariable = 'Steve';
