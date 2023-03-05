bfd23b7185945ac7cf2d6e02f141c3e1f4fb5a36
sakki594 <116419266+sakki594@users.noreply.github.com>
2023-03-05 16:55:42 +0900

    Update main.js

--- sakki/test-site/scripts/main.js
+++ sakki/test-site/scripts/main.js
@@ -1,18 +1,20 @@
 const myHeading = document.querySelector('h1');
-myHeading.textContent = '・・・　練習用page　・・・';
+myHeading.textContent = '・・・　Practice Page　・・・';
 
 //変数の宣言をする
 let iceCream;
 iceCream = 'バニラ';
 
+/*
 if (iceCream === 'チョコレート') {
-  alert('やった、チョコレートアイス大好き！');
+  alert('やった、チョコレートアイス大好き🍨🍫');
 } else {
-  alert('私のお気に入りは....チョコレート🍫');
+  alert('私のお気に入りは....チョコレート🍫💗');
 }
+*/
 
 let myVar = document.querySelector('h1');
-alert('ようこそ！練習ページへ！');
+alert('ようこそ！練習ページへ💻👓');
 
 document.querySelector('html').addEventListener('click', () => {
   alert('✋✨');
