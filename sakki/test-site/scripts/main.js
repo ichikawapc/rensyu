269d6333a8c91b82e3cd74e68aa21982b08b7975
sakki594 <116419266+sakki594@users.noreply.github.com>
2023-03-18 17:42:54 +0900

    Update main.js

--- sakki/test-site/scripts/main.js
+++ sakki/test-site/scripts/main.js
@@ -3,9 +3,17 @@ const myImage = document.querySelector('img');
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute('src');
   if (mySrc === 'images/anya-icon.png') {
-    myImage.setAttribute('src','images/firefox2.png');
+    myImage.setAttribute('src','images/anya-shock.png');
   } else {
     myImage.setAttribute('src','images/anya-icon.png');
   }
 }
 
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  const myName = prompt('あなたの名前を入力してください。');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = `Mozilla はかっこいいよ、${myName}`;
+}
