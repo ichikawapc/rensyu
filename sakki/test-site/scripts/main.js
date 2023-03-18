4973eb8d89d0b0723f17edbb01a454702ef0a311
sakki594 <116419266+sakki594@users.noreply.github.com>
2023-03-18 17:33:27 +0900

    Update main.js

--- sakki/test-site/scripts/main.js
+++ sakki/test-site/scripts/main.js
@@ -2,10 +2,10 @@ const myImage = document.querySelector('img');
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute('src');
-  if (mySrc === 'images/firefox-icon.png') {
+  if (mySrc === 'images/anya-icon.png') {
     myImage.setAttribute('src','images/firefox2.png');
   } else {
-    myImage.setAttribute('src','images/firefox-icon.png');
+    myImage.setAttribute('src','images/anya-icon.png');
   }
 }
 
