6dfaa06ad994724d1c5c2474c2e2139988089b16
bboommuu <>
2023-03-18 17:21:12 +0900

    no message

--- bomu/test-site/scripts/main.js
+++ bomu/test-site/scripts/main.js
@@ -1,5 +1,5 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+//const myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!';
 
 
 //HTMLを実行して10秒経過したら、「10秒経過しました」と表示する。
@@ -11,7 +11,43 @@ function dispMsg(){
 
 
 //ウェブページをクリックすると、表示する。
-document.querySelector('html').addEventListener('click', function () {
-    alert('痛っ! つつかないで!');
-  });
+//document.querySelector('html').addEventListener('click', function () {
+//    alert('痛っ! つつかないで!');
+//  });
   
+
+//画像を入れ替える
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute('src','images/microsoft-edge.png');
+  } else {
+    myImage.setAttribute('src','images/firefox-icon.png');
+  }
+}
+
+//タイトルにユーザーネームを入れる
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    const myName = prompt('あなたの名前を入力してください。');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = `Mozilla はかっこいいよ、${myName}`;
+    }
+  }
+  if (!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem('name');
+    myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  }
+      
