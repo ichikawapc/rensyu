1c92e1e01b625b225d673e19e604f41408142846
makkuroshippo <143084216+makkuroshippo@users.noreply.github.com>
2023-09-16 16:02:01 +0900

    Update main.js

--- makkuroshippo/test-site/scripts/main.js
+++ makkuroshippo/test-site/scripts/main.js
@@ -3,6 +3,6 @@ myHeading.textContent = "Hello world!";
 
 alert("Hi! :o");
 
-document.querySelector("index.html").addEventlistner("click", () => {
+document.querySelector("index.html").addEventListner("click", () => {
   alert("呼んだ?:)");
 });
