d7c734039bf478fd07cceb85521fdfbbbafa058c
rubikku <>
2024-01-05 16:59:23 +0900

    クリックできるようにするため

--- ruubikku/js-rensyu/scripts/main.js
+++ ruubikku/js-rensyu/scripts/main.js
@@ -4,11 +4,13 @@ let a = 0;
 let cli = 1;
 let po = 50;
 let o = 1;
-let oot = 0;
+let ootip = 0;
 let ii = 0;
+let ootpo = 200;
 window.addEventListener('DOMContentLoaded', function(){
     this.setInterval(() => {
-        a = a + oot/10;
+        a = a + ootip/10;
+        console.log(a);
         ii = Math.floor(a*10)/10;
         let g = this.document.getElementById("ku");
         g.innerHTML=ii+" point"
@@ -31,5 +33,18 @@ function clicke(){
         di.innerHTML=po+"point = +"+o
         let aa = document.getElementById("ko");
         aa.innerHTML="1click = "+cli
+    }
+}
+function clickw(){
+    if (ootpo<=a){
+        ootip = ootip + 1;
+        a = a - ootpo;
+        ootpo = Math.floor(ootpo * 2.5);
+        let dis = document.getElementById("ku");
+        dis.innerHTML=a+"point"
+        let di = document.getElementById("oot");
+        di.innerHTML=ootpo+"point = +1"
+        let oot = document.getElementById("ooto");
+        oot.innerHTML="1s = "+ootip
     }
 }
